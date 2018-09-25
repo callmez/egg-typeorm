48e6edb1c0b529bea77e928fcda613a48787620a
CallMeZ <callme-z@qq.com>
2018-09-25 10:25:39 +0800

    clean

--- index.ts
+++ index.ts
@@ -1,18 +1,15 @@
-export * from './lib/decorators';
-export * from './lib/interfaces';
-
 import { EggTypeormOptions } from './lib/interfaces';
 import { Connection } from 'typeorm';
 import 'egg';
+
+export * from './lib/decorators';
+export * from './lib/interfaces';
 
 declare module 'egg' {
   // extend app
   interface Application {
     typeorm: Connection;
   }
-
-  // extend context
-  interface Context {}
 
   // extend your config
   interface EggAppConfig {
